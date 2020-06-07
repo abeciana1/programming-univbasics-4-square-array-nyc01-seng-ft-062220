6e42e2d159175d8465a956b0ef782f3512f1a754
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 18:58:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,13 @@
 def square_array(array)
+  count = 0
   
   new_array = []
   
-  while  do
+  while array.length == array.length do
     
-  new_array = array ** 2
+  new_array[count] = array[count] ** 2
+  
+  count += 1
 end
   return new_array
 end
