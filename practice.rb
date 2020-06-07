5c1ced2dc21845c92878735683c5fd35e69a1b3f
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 18:55:16 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,12 +1,15 @@
 
 
 def square_array(array)
+  count = 0
   
   new_array = []
   
-  while array do
+  array.lenght.times array[count] do
     
-  new_array = array ** 2
+  new_array[count] = [count] ** 2
+  
+  count += 1
 end
   return new_array
 end
