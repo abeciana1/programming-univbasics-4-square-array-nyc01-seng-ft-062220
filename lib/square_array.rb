8471891c405a1ecf3438dafd64d587fce3b55343
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 19:10:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,11 @@
 def square_array(array)
   count = 0 
   
-  #new_numbers = []
+  new_numbers = []
   
   while count < array.length do
-  puts array[count]**2
+  puts new_numbers == array[count]**2
   count += 1
 end
- 
- #new_numbers
+ new_numbers
 end
