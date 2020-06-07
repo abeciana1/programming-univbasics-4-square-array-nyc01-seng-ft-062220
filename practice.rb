6bc7059e14293ed55ed16f0720cde320c4b63c64
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 19:08:43 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -3,13 +3,13 @@ numbers = [2, 3, 4]
 def square(array)
   count = 0 
   
-  #new_numbers = []
+  new_numbers = []
   
   while count < array.length do
   puts array[count]**2
   count += 1
 end
-# new_numbers
+ new_numbers
 end
 
 square(numbers)
