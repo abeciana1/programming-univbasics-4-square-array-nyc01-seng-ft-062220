f8bc7e9df06fd14007537166ba26b3c381a1349c
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 19:04:53 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,17 +1,1 @@
-array_1 = [1, 3, 4]
-
-def square_array(array)
-  count = 0
-  
-  new_array = []
-  
-  while array.length == array.length do
-    
-  new_array[count] = array[count] ** 2
-  
-  count += 1
-end
-  return new_array
-end
-
-puts square_array(array_1)
+numbers = [2, 3, 4]
